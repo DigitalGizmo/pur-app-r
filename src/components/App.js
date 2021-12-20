287639b350a4e529ed49b2a338a0ed53593c1d05
Juliet Jacobson <julietanna@users.noreply.github.com>
2021-12-20 14:52:31 -0500

    tweaked styles for dauner

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import './App.css';
 function App() {
   return (
     <div >
-      <ul id="main-menu">
-        <li className="little-title">Picturing Urban Renewal: </li>
-        <li><a href="http://app.picturingurbanrenewal.org/">Home | </a></li>
-        <li><a href="http://app.picturingurbanrenewal.org/cities">4 Cities / 4 Stories</a> | </li>
-        <li><a href="http://app.picturingurbanrenewal.org/people">Personal Stories</a> | </li>
-        <li><a href="http://app.picturingurbanrenewal.org/visuals">Visual Record</a> | </li>
-        <li><a href="http://app.picturingurbanrenewal.org/places">Places</a></li>  
-      </ul>
-  
+      <nav class="main-menu">
+        <ul>
+          <li><a href="http://app.picturingurbanrenewal.org/">Home | </a></li>
+          <li><a href="http://app.picturingurbanrenewal.org/cities">4 Cities / 4 Stories</a> | </li>
+          <li><a href="http://app.picturingurbanrenewal.org/people">Personal Stories</a> | </li>
+          <li><a href="http://app.picturingurbanrenewal.org/visuals">Visual Record</a> | </li>
+          <li><a href="http://app.picturingurbanrenewal.org/places">Places</a></li>  
+        </ul>
+      </nav>
       <Outlet />
     </div>
   );
