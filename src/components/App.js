bc1e86957a1e2f505c181ab571c6690c3efec9ba
Don Button <donpublic@digitalgizmo.com>
2021-12-20 15:10:28 -0500

    remove extra escape chars

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ function App() {
     <div >
       <nav class="main-menu">
         <ul>
-          <li><a href="http://app.picturingurbanrenewal.org/">Home | </a></li>
-          <li><a href="http://app.picturingurbanrenewal.org/cities">4 Cities / 4 Stories</a> | </li>
-          <li><a href="http://app.picturingurbanrenewal.org/people">Personal Stories</a> | </li>
-          <li><a href="http://app.picturingurbanrenewal.org/visuals">Visual Record</a> | </li>
+          <li><a href="http://app.picturingurbanrenewal.org/">Home </a></li>
+          <li><a href="http://app.picturingurbanrenewal.org/cities">4 Cities / 4 Stories</a> </li>
+          <li><a href="http://app.picturingurbanrenewal.org/people">Personal Stories</a> </li>
+          <li><a href="http://app.picturingurbanrenewal.org/visuals">Visual Record</a> </li>
           <li><a href="http://app.picturingurbanrenewal.org/places">Places</a></li>  
         </ul>
       </nav>
--- src/components/people/SlideShow.js
+++ src/components/people/SlideShow.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './SlideShow.css';
 
 const slides = [
-  {imgName: 'dauner-arch-details', caption: 'Architectural details of a building soon to be demolished for Kingston\'\s Broadway East Urban Renewal Project, 1967.'},
+  {imgName: 'dauner-arch-details', caption: 'Architectural details of a building soon to be demolished for Kingston\'s Broadway East Urban Renewal Project, 1967.'},
   {imgName: 'dauner-1865-bldg', caption: 'The 1865 Building, 74 Broadway, before demolition.'},
   {imgName: 'dauner-1865-bldg-demo', caption: 'The 1865 Building being demolished.'},
   {imgName: 'haines-mcmillan-bldg', caption: 'Demolition of McMillan Building, Downtown Kingston, 1967.'},
   {imgName: 'haines-old-city-hall-pigeons', caption: 'This Bob Haines photograph accompanied an article by Kingston Daily Freeman Reporter Hugh Reynolds, March 18, 1975.'},
   {imgName: 'dauner-demo-scene', caption: 'Aftermath of demolition.'},
   {imgName: 'dauner-hasbrouck-strand', caption: 'Corner of Hasbrouck St. and E. Strand. The building on the left is the McMillian Building, former home of ship chandlery, from which Gene Dauner and Bob Haines removed some architectural detail. The former Colonial Hotel is across the street.'},
-  {imgName: 'dauner-poor-mans-paradise', caption: 'Poor Man\'\s Paradise in process of demolition This secondhand store at 65 E. Strand was owned by Nick Matthews.'},
-  {imgName: 'dauner-yallums-detail', caption: 'Close-up photograph of Yallum\'\s Department Store. The x inside a circle is a common way to indicate that a vacant building was dangerous to enter.'},
+  {imgName: 'dauner-poor-mans-paradise', caption: 'Poor Man\'s Paradise in process of demolition This secondhand store at 65 E. Strand was owned by Nick Matthews.'},
+  {imgName: 'dauner-yallums-detail', caption: 'Close-up photograph of Yallum\'s Department Store. The x inside a circle is a common way to indicate that a vacant building was dangerous to enter.'},
   {imgName: 'haines-buildings-boarded-up-2', caption: 'A row of buildings boarded up and awaiting demolition in the Broadway East urban renewal area. Bob Haines photo.'},
   {imgName: 'haines-clearance', caption: 'Broadway East urban renewal area in process of being cleared. Bob Haines photograph.'},
   {imgName: 'haines-demo-rear-bldg', caption: 'Rear of building being demolished. Bob Haines photograph.'},
