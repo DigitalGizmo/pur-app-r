f83e5c34b5732bd8fca1581a71b052e424111866
Don Button <donpublic@digitalgizmo.com>
2022-01-12 18:03:54 -0500

    add image list to Visuals

--- src/components/archive/Visuals.js
+++ src/components/archive/Visuals.js
@@ -1,66 +1,66 @@
 import React from 'react';
+import {
+    ApolloClient,
+    InMemoryCache,
+    // ApolloProvider,
+    useQuery,
+    gql
+} from "@apollo/client"
+import './Visuals.css';
 
 
-// import {
-//     // ApolloClient,
-//     // InMemoryCache,
-//     // ApolloProvider,
-//     useQuery,
-//     gql
-// } from "@apollo/client"
+const client = new ApolloClient({
+  uri: 'http://admin.picturingurbanrenewal.org/archive/graphql/',
+  cache: new InMemoryCache()
+});
 
-// //   const client = new ApolloClient({
-// //     uri: 'http://admin.picturingurbanrenewal.org/archive/graphql/',
-// //     cache: new InMemoryCache()
-// //   });
+const Visuals= () => {
 
-// const ALL_IMAGES = gql`
-//   query getImages {
-//     all_images {
-//       slug
-//     }
-//   }
-// `
-// const { loading, error, data } = useQuery(ALL_IMAGES);
-
-  // client
-  //   .query({
-  //       query: gql `
-  //           query getImages {
-  //             all_images{
-  //               slug
-  //             }
-  //           }
-  //       `
-  //   })
-  //   .then(result => console.log(result.data.all_images));
-
-// const Visuals = () => {
-
-//   const { loading, error, data } = useQuery(ALL_IMAGES);
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error :(</p>;
-
-//   return (
-//     <div>
-//       <p>Found { this.data.all_images.length} images</p>
-
-//     </div>
-//   );
-
-// }
-
-class Visuals extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <p>Hello Apollo!</p>
-                <p>Found </p>
-            </div>
-        );
+  const { loading, error, data } = useQuery(gql`
+    query getImages {
+      all_images {
+        id
+        slug
+        title
+        description
+        creation_year
+        source_title
+        city
+        district_title
+        street_address
+      }
     }
+  `
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  // return data.all_images.map(({ id, slug, title }) => (
+  const images = data.all_images.map(({ id, slug, title }) => {
+    return (
+      <figure 
+        className="images-menu-item"
+        key={id}
+        >
+          <img className="menu-image" 
+                src={`http://admin.picturingurbanrenewal.org/media/visuals/thumbpics/${slug}-tn.jpg`}
+                alt={title}
+          />
+          <figcaption className="menu-title">
+            -- { title}
+          </figcaption>
+      </figure>
+    )
+  });
+
+  return (
+    <div>
+      <p>Hello Apollo!</p>
+      <p>Found function based component</p>
+      <div className="images-menu">{images}</div>
+    </div>
+  );
 }
 
 export default Visuals;
