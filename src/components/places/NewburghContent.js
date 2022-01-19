a70518b447b699efd29406d0543c3c458e9343f2
Don Button <donpublic@digitalgizmo.com>
2022-01-19 17:03:24 -0500

    multiple captions, images
    using InView. Not yet able to map

---
+++ src/components/places/NewburghContent.js
@@ -0,0 +1,55 @@
+const images = [
+  'newburgh-aerial-1-title',
+  'newburgh-aerial-2-title',
+  'newburgh-guided-map1-title',
+  'newburgh-guided-map1a-title',
+  'newburgh-guided-map1b-title',
+  'newburgh-front&water',
+  'Feasability_Survey_Area_Map3-sized',
+  'Feasability_Survey_Area_Map3-deterior',
+  'newburgh-map3-5a',
+  'newburgh-map3-5b',
+  'newburgh-map3-5c',
+  '196GrandSt-hodges-house',
+  'Williams-Montgomery-St-96-98',
+  'newburgh-guided-map4',
+  'newburgh-guided-map4',
+  'newburgh-guided-map4',
+  'newburgh-map-2',
+  'newburgh-map-15',
+  'newburgh-guided-map10',
+  'newburgh-map-20',
+  'newburgh-map-20',
+  'newburgh-map-20',
+  'newburgh-map-20',
+  'newburgh-map19',
+  'RRtrestle-WaterStGone',
+];
+
+  const contents = [
+    {
+      label: 'text-1a',
+      title: 'The View from Above',
+      text: '<p>Looking down on East Newburgh, urban planners saw blighted buildings, obsolete land-use patterns, and an outdated street grid. The city&rsquo;s population, as well as its tax base, was in decline. The planners&rsquo; solution to this cluster of problems was to start over from scratch.</p>',
+    },
+    {
+      label: 'text-1b',
+      title: '',
+      text: '<p>This was the logic of urban renewal: Demolition of existing homes and shops was the first step toward transforming a blighted area. Once cleared, small lots could be repackaged into larger parcels for sale to private developers. Those developers would build modern housing and commercial developments. New people (whiter and wealthier than their predecessors) would move into the new homes. New businesses would rent space in the commercial structures. These new businesses and residents would stabilize municipal finances.</p>'
+    },
+    {
+      label: 'text-2a',
+      title: 'The Appraiser&rsquo;s Advice',
+      text: '<p>In 1964, when Newburgh&rsquo;s city council hired the planning firm of David Rosen Associates, urban renewal was already underway.</p>'
+    },
+    {
+      label: 'text-2b',
+      title: '',
+      text: '<p>The planning firm&rsquo;s job was to assess the &ldquo;feasibility&rdquo; of expanding the urban renewal area.</p>'
+    },
+
+  ]
+
+
+export {images}
+export {contents}
