c6622f692b1ab9ab8b4beef039285dd208727805
Don Button <donpublic@digitalgizmo.com>
2022-02-01 17:22:21 -0500

    attempted visuals var width rows

--- src/components/archive/ImageCard.js
+++ src/components/archive/ImageCard.js
@@ -13,9 +13,11 @@ class ImageCard extends React.Component {
   }
   
   setSpans = () => {
-    const height = this.imageRef.current.clientHeight;
-    const spans = Math.ceil(height / (15 + 3));
-    // console.log('h: ' + height + ' spans: ' + spans);
+    // const height = this.imageRef.current.clientHeight;
+    const width = this.imageRef.current.clientWidth;
+    // const spans = Math.ceil(height / 15);
+    const spans = Math.ceil(width / (15 + 3));
+    console.log('w: ' + width + ' spans: ' + spans);
     this.setState({ spans: spans });
   }  
 
@@ -23,7 +25,8 @@ class ImageCard extends React.Component {
     const { slug } = this.props.image; //, title
 
     return (
-      <div style ={{ gridRowEnd: `span ${this.state.spans}` }}>
+      // <div style ={{ gridRowEnd: `span ${this.state.spans}` }}>
+      <div style ={{ gridColumnEnd: `span ${this.state.spans}` }}>
         <img
           ref={this.imageRef}
           alt="{title}"
