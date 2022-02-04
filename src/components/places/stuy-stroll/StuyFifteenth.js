0daac17b16686cee94308af257e72fce4c99a7c7
Juliet Jacobson <julietanna@users.noreply.github.com>
2022-02-04 12:04:15 -0500

    added intro text panel

--- src/components/places/stuy-stroll/StuyFifteenth.js
+++ src/components/places/stuy-stroll/StuyFifteenth.js
@@ -4,7 +4,7 @@ const StuyFifteenth = ({onPageChange}) => {
 
   return (
     <div id="stroll-wrapper">
-      <div>
+      <div className="intro-page">
 
         <svg 
           version="1.1" 
@@ -32,6 +32,13 @@ const StuyFifteenth = ({onPageChange}) => {
 
         </svg>
 
+        <article className="intro-panel">
+          <p>In April 1943, residents of New York&rsquo;s storied Gas House District&mdash;named for the Consolidated Edison gas works on its northwest border&mdash;learned they would soon lose their homes. Businesses, likewise, would have to move. In their place, Metropolitan Life Insurance Co. planned to build a massive new housing complex, known as Stuyvesant Town. And the corporate giant had city leaders on their side.</p>
+ 
+          <p>Many residents and business owners fought to save their neighborhood and lost. But the district survives in photographs from the Met Life archive.</p>
+ 
+          <p>Join us for a jaunt down 1st Avenue and around the block to 14th Street. Stop and see the sights we&rsquo;ve spotlighted along the way. Click the spotlight or the list of topics along the street to take a closer look and learn more.</p>
+        </article>
       </div>
     </div>
   )
