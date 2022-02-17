4d8fe5373095cc80d2b3ac7795a15135b67bf3a0
Don Button <donpublic@digitalgizmo.com>
2022-02-17 16:51:47 -0500

    radio filter for formats

--- src/components/archive/Visuals.js
+++ src/components/archive/Visuals.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react'; // , { useState }
+import React, { useState, useEffect } from 'react';
 import {
     useQuery,
     gql
-} from "@apollo/client"; // , makeVar
+} from "@apollo/client"; 
 import './Visuals.css';
 import ImageList from './ImageList';
 
 const Visuals= () => {
   const [cityID, setCityID] = useState(0);
-  // const [numImages, setNumImages] = useState(0);
+  const [formatID, setFormatID] = useState(0);
+  const [numImages, setNumImages] = useState(0);
 
   const onCityChange = (event) => {
     setCityID(parseInt(event.target.value));
   }
 
+  const onFormatChange = (event) => {
+    setFormatID(parseInt(event.target.value));
+  }
+
   const GET_IMAGE_LIST = gql`
-    query getImages ($city_id: Int) {
-      visualRecord (cityId: $city_id) {
+    query getImages ($city_id: Int, $media_format_id: Int) {
+      visualRecord (cityId: $city_id, mediaFormatId: $media_format_id) {
         slug
         title
         description
@@ -36,10 +41,14 @@ const Visuals= () => {
   `;
 
   const { loading, error, data } = useQuery(
-    GET_IMAGE_LIST, { variables: { city_id: cityID } }
+    GET_IMAGE_LIST, { variables: { city_id: cityID, media_format_id: formatID } }
   );
 
-  // console.log('num images: ' + data.visualRecord.length);
+  useEffect(() => {
+    if (typeof data !== 'undefined') {
+      setNumImages(data.visualRecord.length);
+    }
+  }, [data])
 
   const CITY_RADIO = [
     { ID: "city-albany", value: 5, label: "Albany"},
@@ -63,11 +72,33 @@ const Visuals= () => {
     )    
   });
 
+  const FORMAT_RADIO = [
+    { ID: "format-photo", value: 1, label: "Photographs"},
+    { ID: "format-graphgic", value: 2, label: "Graphics/Ephemera"},
+    { ID: "format-maps", value: 3, label: "Maps"},
+    { ID: "format-personal", value: 6, label: "Personal Documents"},
+    { ID: "format-published", value: 7, label: "Published Docuements"},
+    { ID: "format-legal", value: 8, label: "Legal Documents"},
+  ];
+
+  const formats = FORMAT_RADIO.map((format) => {
+    return (
+      <li key={format.ID}>
+        <input type="radio" name="format"
+          id={format.ID} value={format.value}
+          checked={ formatID === format.value } 
+          onChange={onFormatChange} 
+        />
+        <label htmlFor={format.ID}>{format.label}</label>
+      </li>
+    )    
+  });
+
   return (
     <div> 
       <header className="basic-page">
         <h1>The Visual Record</h1>
-        <h4># Images | Filter Images</h4>
+        <h4>{numImages} Images | Filter Images</h4>
       </header>
 
       <div className="filters">
@@ -76,19 +107,23 @@ const Visuals= () => {
         <h4>Era</h4>
         <ul class="filter-set">
           <li>
-            <input type="checkbox" id="era-1940s" name="1940s"/>
+            <input type="checkbox" id="era-1940s" name="1940s"
+              disabled/>
             <label for="era-1940s">1940s</label>
           </li>
           <li>
-            <input type="checkbox" id="era-1950s" name="1950s"/>
+            <input type="checkbox" id="era-1950s" name="1950s"
+              disabled/>
             <label for="era-1950s">1950s</label>
           </li>
           <li>
-            <input type="checkbox" id="era-1960s" name="1960s"/>
+            <input type="checkbox" id="era-1960s" name="1960s"
+              disabled/>
             <label for="era-1960s">1960s</label>
           </li>
           <li>
-            <input type="checkbox" id="era-1970s" name="1970s"/>
+            <input type="checkbox" id="era-1970s" name="1970s"
+              disabled/>
             <label for="era-1970s">1970s</label>
           </li>
         </ul>
@@ -106,23 +141,28 @@ const Visuals= () => {
           <h4>Topic</h4>
           <ul class="filter-set">
             <li>
-              <input type="checkbox" id="topic-demolition" name="demolition"/>
+              <input type="checkbox" id="topic-demolition" name="demolition"
+              disabled/>
               <label for="topic-demolition">Demolition</label>
             </li>
             <li>
-              <input type="checkbox" id="" name="topic-plans"/>
+              <input type="checkbox" id="" name="topic-plans"
+              disabled/>
               <label for="topic-plans">Plans</label>
             </li>
             <li>
-              <input type="checkbox" id="" name="topic-people"/>
+              <input type="checkbox" id="" name="topic-people"
+              disabled/>
               <label for="topic-people">People/Daily Life</label>
             </li>
             <li>
-              <input type="checkbox" id="" name="topic-arch"/>
+              <input type="checkbox" id="" name="topic-arch"
+              disabled/>
               <label for="topic-arch">Architecture</label>
             </li>
             <li>
-              <input type="checkbox" id="" name="topic-before"/>
+              <input type="checkbox" id="" name="topic-before"
+              disabled/>
               <label for="topic-before">Before Redevelopment</label>
             </li>
           </ul>
@@ -131,26 +171,7 @@ const Visuals= () => {
         <div>
           <h4>Format</h4>
           <ul class="filter-set">
-            <li>
-              <input type="checkbox" id="format-photos" name="photos"/>
-              <label for="format-photos">Photographs</label>
-            </li>
-            <li>
-              <input type="checkbox" id="format-graphics" name="graphics"/>
-              <label for="format-graphics">Graphics/Ephemera</label>
-            </li>
-            <li>
-              <input type="checkbox" id="format-drawings" name="drawings"/>
-              <label for="format-drawings">Architectural Drawings</label>
-            </li>
-            <li>
-              <input type="checkbox" id="format-maps" name="maps"/>
-              <label for="format-maps">Maps</label>
-            </li>
-            <li>
-              <input type="checkbox" id="format-docs" name="documents"/>
-              <label for="format-docs">Documents</label>
-            </li>
+            { formats }
           </ul>
         </div>
       </div>
