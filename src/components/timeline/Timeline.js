c6897fd7b203560fc02f645283f90b4be9aaf08f
Juliet Jacobson <julietanna@users.noreply.github.com>
2022-02-25 14:55:30 -0500

    first draft layout
    problem: left column not fixed cuz fixed and grid don't work together.

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Timeline.css';
 
 const Timeline = () => {
   return (
     <div> 
       <header className="basic-page">
         <h1>Timeline</h1>
-        <h4>Subtitle</h4>
       </header>
 
-      <div>
-        <p>Timeline goes here!</p>
+      <div className="timeline-page">
+
+        <section className="thru-lines">
+          <ul>
+            <li className="title">Through Lines</li>
+            <li>Urban Planning</li>
+            <li>Demolition and Displacement</li>
+            <li>Housing</li>
+            <li>Reconstruction</li>
+            <li>Preservation</li>
+            <li>Protest</li>
+            <li>Civil Rights</li>
+            <li>Support for Urban Renewal</li>
+          </ul>
+        </section>
+
+        <h4 className="ny">New York City</h4>
+        <h4 className="newburgh">Newburgh</h4>
+        <h4 className="kingston">Kingston</h4>
+        <h4 className="albany">Albany</h4>
+        <h4 className="us">NY &amp; US</h4>
+
+        <section className="timeline-content">
+          <p>timeline content goes here</p>
+        </section>
+
       </div>
     </div>
   )
