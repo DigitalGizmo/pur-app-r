755b3c6e6f7b3f164074b9b365fcf927103051f6
Don Button <donpublic@digitalgizmo.com>
2022-02-27 12:18:21 -0500

    down to five topics

--- src/components/archive/Topics.js
+++ src/components/archive/Topics.js
@@ -4,22 +4,11 @@ import CheckboxesById from './CheckboxesById';
 const Topics = ({topicIDs, setTopicIDs}) => {
   
   const ITEMS = [
-    { ID: "topic-architecture", value: 4, label: "Architecture"},
-    { ID: "topic-contract-sale", value: 11, label: "Contract Sale"},
-    { ID: "topic-demolition", value: 1, label: "Demolition"},
-    { ID: "topic-deterioration", value: 9, label: "Deterioration"},
-    { ID: "topic-preservation", value: 8, label: "Historic Preservation"},
-    { ID: "topic-opposition", value: 12, label: "Opposition to urban renewal plans or relocation"},
+    { ID: "topic-architecture", value: 4, label: "Pre-Demolition"},
+    { ID: "topic-planning", value: 15, label: "Plannning"},
     { ID: "topic-daily-life", value: 3, label: "People, Daily Life"},
-    { ID: "topic-planning", value: 15, label: "Plannning"},   
-    { ID: "topic-acquisition", value: 10, label: "Property Acquisition"}, 
-    { ID: "topic-reconstruction", value: 5, label: "Reconstruction"},       
-    { ID: "topic-relocation", value: 6, label: "Relocation of residents or businesses"},
-    { ID: "topic-discrimination", value: 14, label: "Residential discrimination"},
-    { ID: "topic-support", value: 13, label: "Supports urban renewal"},
-    { ID: "topic-outreach", value: 7, label: "Urban renewal publicity and outreach"}, 
-    { ID: "topic-vision", value: 2, label: "Vision for Redevelopment"},  
-
+    { ID: "topic-demolition", value: 1, label: "Demolition"},
+    { ID: "topic-reconstruction", value: 5, label: "Reconstruction"},
   ];
 
   return (
