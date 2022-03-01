56b9170eb8c438fba62213cc401f1c807205b45c
Don Button <donpublic@digitalgizmo.com>
2022-03-01 15:12:14 -0500

    initial table loop drop

--- src/components/timeline/TimelineTable.js
+++ src/components/timeline/TimelineTable.js
@@ -1,27 +1,48 @@
 import React from "react";
 import "./TimelineContent";
-import { timelineContent } from "./TimelineContent";
-import './TempTable.css';
-
+import { years, timelineContent } from "./TimelineContent";
+import './Timeline.css';
 
 const TimelineTable = () => {
 
   return (
-    <table className="temp">
-      <row>
+    <table className="timeline-content">
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          {years.map((year) => (
+          <th>{year}</th>
+        ))}
+        </tr>
+      </thead> 
+       
+      <tbody>
+
+        {timelineContent.map ((row) => (
+          <tr className="row-nyc">
+            <th>{row.city}</th>
+            {row.years.map((content) => (
+              <td>{content.text}</td>
+            ))}
+          </tr>
+        ))}        
+
+      </tbody>  
+
+      {/* <row>
         <td>City</td>
         {timelineContent[0].years.map((head) => (
           <td>{head.year}</td>
         ))}
-      </row>
-      {timelineContent.map ((row) => (
+      </row> */}
+      {/* {timelineContent.map ((row) => (
         <row className="temp">
           <td>{row.city}</td>
           {row.years.map((content) => (
           <td>{content.text}</td>
         ))}
         </row>
-      ))}
+      ))} */}
     </table>
   )
 
