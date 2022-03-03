7468b1b4114ff18ce04db97448c61dbca5f8c196
Don Button <donpublic@digitalgizmo.com>
2022-03-03 15:01:18 -0500

    add Figma timeline iFrame

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TimelineTable from './TimelineTable';
+// import TimelineTable from './TimelineTable';
 import './Timeline.css';
 
 const Timeline = () => {
@@ -25,8 +25,15 @@ const Timeline = () => {
           </ul>
         </section>
 
-        <TimelineTable
-        />
+        {/* <TimelineTable
+        /> */}
+
+        <iframe 
+          style={{border: `1px solid rgba(0, 0, 0, 0.1)`}}  
+          width="1400" height="1000" 
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FXBfNOWDi9ewcaKrJv0Lv4q%2FTimeline%3Fnode-id%3D2%253A2%26scaling%3Dmin-zoom%26page-id%3D0%253A1%26starting-point-node-id%3D2%253A2" 
+          allowFullScreen>
+        </iframe>
 
       </div>
     </div>
