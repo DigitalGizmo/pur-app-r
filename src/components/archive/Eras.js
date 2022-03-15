4643c2ec75c92d70ff47fdd319e097be7f1aaf8d
Don Button <donpublic@digitalgizmo.com>
2022-03-15 14:29:53 -0400

    expand Eras

--- src/components/archive/Eras.js
+++ src/components/archive/Eras.js
@@ -4,10 +4,12 @@ import CheckboxesById from './CheckboxesById';
 const Eras = ({eraIDs, setEraIDs}) => {
   
   const ITEMS = [
-    { ID: "era-1940s", value: 1, label: "1940s"},
-    { ID: "era-1950s", value: 2, label: "1950s"},
-    { ID: "era-1960s", value: 3, label: "1960s"},
-    { ID: "era-1970s", value: 4, label: "1970s"},
+    { ID: "era-1940s", value: 1, label: "Before 1940s"},
+    { ID: "era-1940s", value: 2, label: "1940s"},
+    { ID: "era-1950s", value: 3, label: "1950s"},
+    { ID: "era-1960s", value: 4, label: "1960s"},
+    { ID: "era-1970s", value: 5, label: "1970s"},
+    { ID: "era-1970s", value: 6, label: "After 1970s"},
   ];
 
   return (
