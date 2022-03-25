d6c8e2cc4d1694467526585846171ca0baf58f0e
Don Button <donpublic@digitalgizmo.com>
2022-03-25 15:48:55 -0400

    trial framer city menu

--- src/components/cities/CitiesMenu.js
+++ src/components/cities/CitiesMenu.js
@@ -10,6 +10,17 @@ const CitiesMenu = () => {
       <ul className="list-menu">
         <li><a href="http://dev.picturingurbanrenewal.org/wireframe/city-stories/albany-displaced.html">Albany: Lost Places/Displaced People - The Reaction to Redevelopment</a></li>
       </ul>
+
+
+      <iframe 
+        style={{border: `1px solid rgba(0, 0, 0, 0.1)`}} 
+        width="640" height="480" 
+        src="https://framer.com/embed/Scroll-Trigger-Hopefully--hbA7M8KLJF1SERQjjP4i/IrcEX2DOx?highlights=0" 
+        >
+      </iframe>
+
+
+
     </div>
   )
 }
