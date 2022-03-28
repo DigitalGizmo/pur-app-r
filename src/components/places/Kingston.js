fc0de8e2ea8331ac2d33532cdca23bcda873ae94
Don Button <donpublic@digitalgizmo.com>
2022-03-28 11:08:43 -0400

    add city grid framer test

--- src/components/places/Kingston.js
+++ src/components/places/Kingston.js
@@ -12,6 +12,13 @@ const Kingston = () => {
         <h4>to be developed!</h4>
       </header>  
 
+      <iframe 
+        style={{border: `1px solid rgba(0, 0, 0, 0.1)`}}
+        width="1200" height="900" 
+        src="https://framer.com/embed/City-Story-Menu--hbA7M8KLJF1SERQjjP4i/IrcEX2DOx?highlights=0" 
+      >
+
+      </iframe>
 
     </div>
   );
