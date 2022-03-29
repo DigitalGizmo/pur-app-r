f9d3b2c3c0c83ea54f9ce517b1b7a327f213b78b
Juliet Jacobson <julietanna@users.noreply.github.com>
2022-03-29 18:00:58 -0400

    Update CitiesMenu.js

--- src/components/cities/CitiesMenu.js
+++ src/components/cities/CitiesMenu.js
@@ -3,23 +3,20 @@ import React from 'react';
 
 const CitiesMenu = () => {
   return (
-    <div className="short-page">
+    <div className="">
       <header className="basic-page">
         <h1>4 Cities / 4 Stories</h1>
       </header>
-      <ul className="list-menu">
+{/*      <ul className="list-menu">
         <li><a href="http://dev.picturingurbanrenewal.org/wireframe/city-stories/albany-displaced.html">Albany: Lost Places/Displaced People - The Reaction to Redevelopment</a></li>
-      </ul>
-
+      </ul>*/}
 
       <iframe 
         style={{border: `1px solid rgba(0, 0, 0, 0.1)`}} 
-        width="640" height="480" 
+        width="1250px" height="1250px" 
         src="https://framer.com/embed/Scroll-Trigger-Hopefully--hbA7M8KLJF1SERQjjP4i/IrcEX2DOx?highlights=0" 
         >
       </iframe>
-
-
 
     </div>
   )
