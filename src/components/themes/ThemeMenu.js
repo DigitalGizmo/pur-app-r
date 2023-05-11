b27e1c5cb4b885a595c6392507bd32f6ea93665c
Juliet Jacobson <julietanna@users.noreply.github.com>
2023-05-11 14:23:08 -0400

    essay work

--- src/components/themes/ThemeMenu.js
+++ src/components/themes/ThemeMenu.js
@@ -25,16 +25,16 @@ const ThemeMenu = () => {
 
       <nav className="horiz-menu">
         <ul>
-          <li className="contract-sales"><Link to="/themes/theme">Buyer Beware: Contract Sales</Link>David Hochfelder, University of Albany, SUNY</li>
-          <li className="who-lived-here"><Link to="/themes/theme-campbell">Who Lived Here? The Lives of Albany Residents Revealed in Photographs</Link>Robin Campbell, University of Albany, SUNY</li>
-          <li className="anti-urban"><Link to="">The Anti-Urban Nature of Urban Renewal</Link>Steven Conn, Miami University</li>
-          <li className="root-shock"><Link to="/themes/theme-fullilove">Root Shock</Link>Mindy Thompson Fullilove, The New School</li>
-          <li className="ghost-city"><Link to="/themes/theme-zipp">Ghost City: MetLife&rsquo;s Gas House District Photos</Link>Samuel Zipp, Brown University</li>
-          <li className="root-shock"><Link to="">What is Blight?</Link>Colin Gordon</li>
-          <li className="ghost-city"><Link to="">Streetscapes: Kingston</Link>William Krattinger</li>
+          <li className="contract-sales"><Link to="/themes/theme"><span className="asterisk">*</span> Buyer Beware: Contract Sales</Link>David Hochfelder, University of Albany, SUNY</li>
+          <li className="who-lived-here"><Link to="/themes/theme-campbell"><span className="asterisk">*</span> Who Lived Here? The Lives of Albany Residents Revealed in Photographs</Link>Robin Campbell, University of Albany, SUNY</li>
+          <li className="ghost-city"><Link to="/themes/theme-zipp"><span className="asterisk">*</span> Ghost City: MetLife&rsquo;s Gas House District Photos</Link>Samuel Zipp, Brown University</li>
+          <li className="root-shock"><Link to="/themes/theme-fullilove"><span className="asterisk">*</span> Root Shock</Link>Mindy Thompson Fullilove, The New School</li>
+          <li className="anti-urban"><Link to="" className="not-done-wrapper"><span className="not-done">Not yet implemented</span>The Anti-Urban Nature of Urban Renewal</Link>Steven Conn, Miami University</li>
+          <li className="blight"><Link to="" className="not-done-wrapper"><span className="not-done">Not yet implemented</span>What is Blight?</Link>Colin Gordon</li>
+{/*          <li className="ghost-city"><Link to="">Streetscapes: Kingston</Link>William Krattinger</li>
           <li className="root-shock"><Link to="">Streetscapes: Stuyvesant Town</Link>William Krattinger</li>
           <li className="ghost-city"><Link to="">Streetscapes: Newburgh</Link>William Krattinger</li>
-          <li className="root-shock"><Link to="">Streetscapes: Albany</Link>William Krattinger</li>
+          <li className="root-shock"><Link to="">Streetscapes: Albany</Link>William Krattinger</li>*/}
         </ul>
       </nav>
     </div>
