09ce4e2dd73e669f1bd8a5a104142d63d8912f93
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-05-12 10:14:38 -0400

    slide titles

--- src/components/cities/IntroSlides.js
+++ src/components/cities/IntroSlides.js
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
 const slides = [
-  {imgName: 'dauner-arch-details', caption: 'Architectural details of a building soon to be demolished for Kingston\'s Broadway East Urban Renewal Project, 1967.'},
-  {imgName: 'dauner-1865-bldg', caption: 'The 1865 Building, 74 Broadway, before demolition.'},
-  {imgName: 'dauner-1865-bldg-demo', caption: 'The 1865 Building being demolished.'},
-  {imgName: 'haines-mcmillan-bldg', caption: 'Demolition of McMillan Building, Downtown Kingston, 1967.'},
-  {imgName: 'haines-old-city-hall-pigeons', caption: 'This Bob Haines photograph accompanied an article by Kingston Daily Freeman Reporter Hugh Reynolds, March 18, 1975.'},
-  {imgName: 'dauner-demo-scene', caption: 'Aftermath of demolition.'},
-  {imgName: 'dauner-hasbrouck-strand', caption: 'Corner of Hasbrouck St. and E. Strand. The building on the left is the McMillian Building, former home of ship chandlery, from which Gene Dauner and Bob Haines removed some architectural detail. The former Colonial Hotel is across the street.'},
-  {imgName: 'dauner-poor-mans-paradise', caption: 'Poor Man\'s Paradise in process of demolition This secondhand store at 65 E. Strand was owned by Nick Matthews.'},
-  {imgName: 'dauner-yallums-detail', caption: 'Close-up photograph of Yallum\'s Department Store. The x inside a circle is a common way to indicate that a vacant building was dangerous to enter.'},
-  {imgName: 'haines-buildings-boarded-up-2', caption: 'A row of buildings boarded up and awaiting demolition in the Broadway East urban renewal area. Bob Haines photo.'},
-  {imgName: 'haines-clearance', caption: 'Broadway East urban renewal area in process of being cleared. Bob Haines photograph.'},
-  {imgName: 'haines-demo-rear-bldg', caption: 'Rear of building being demolished. Bob Haines photograph.'},
-  {imgName: 'haines-demo-unknown-bldgs-3', caption: 'Demolition of buildings in the Broadway East urban renewal area. Bob Haines photograph.'},
-  {imgName: 'haines-fischer-street', caption: 'Fischer Building after demolition, with adjacent buildings awaiting their turn. Bob Haines photograph.'},
-  {imgName: 'haines-utility-poles', caption: 'Utility workers removing wiring and other equipment from overhead poles in the Broadway East urban renewal area. Bob Haines photograph.'},];
+  { imgName: 'dauner-arch-details', 
+    title: 'Thing number 1',
+    caption: 'Architectural details of a building soon to be demolished for Kingston\'s Broadway East Urban Renewal Project, 1967.'
+  },
+  { imgName: 'dauner-1865-bldg', 
+    title: 'Thing number 2',
+    caption: 'The 1865 Building, 74 Broadway, before demolition.'
+  },
+  { imgName: 'dauner-1865-bldg-demo', 
+    title: 'Thing number 3',
+    caption: 'The 1865 Building being demolished.'
+  },
+  { imgName: 'haines-mcmillan-bldg', 
+    title: 'Thing number 4',
+    caption: 'Demolition of McMillan Building, Downtown Kingston, 1967.'
+  },
+  { imgName: 'haines-old-city-hall-pigeons', 
+    title: 'Thing number 5',
+    caption: 'This Bob Haines photograph accompanied an article by Kingston Daily Freeman Reporter Hugh Reynolds, March 18, 1975.'
+  },
+  { imgName: 'dauner-demo-scene', 
+    title: 'Thing number 6',
+    caption: 'Aftermath of demolition.'
+  },
+]
 
 const IntroSlides = (props) => {
   const [currSlide, setCurrSlide] = useState(0);
@@ -61,7 +71,7 @@ const IntroSlides = (props) => {
         </div>
 
         <h3>Visual Intro: NYC A Suburb in the City</h3> {/*props.title*/}
-        <h1>Slide Title Goes Here</h1>
+        <h1>{slides[currSlide].title}</h1>
         <p>{slides[currSlide].caption}</p>
       </header>
         
